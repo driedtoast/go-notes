ab6b09284ac000b50ca9e6279b7a1a6c9954b978
Daniel Marchant <driedtoast@gmail.com>
2013-09-20 13:56:46 -0700

    incrementing changes

--- src/gonotes/gonotes.go
+++ src/gonotes/gonotes.go
@@ -9,10 +9,12 @@ import (
 
 // Flag expected from the commmand line
 var createFlag = flag.Bool("c", false, `Create file \n`)
+var tagFlag = flag.String("t", "default", `Tag the note with values / Search notes based on list option \n`)
+var listFlag = flag.Bool("l", true, `Lists the notes in storage\n`)
 
 // TODO integrate with this
 var createMigration = flag.Bool("m", false, `Create migration / schema file \n`)
-var lineNumbers = flag.Int("l", 100, `Number of lines to save to notes \n`)
+var lineNumbers = flag.Int("n", 100, `Number of lines to save to notes \n`)
 
 // TODO::
 // create a flag for type of note / line storage
@@ -21,16 +23,23 @@ var lineNumbers = flag.Int("l", 100, `Number of lines to save to notes \n`)
 // call commands based on flags
 // clean up command line parsing
 
+type CommandInput struct {
+	tags string
+	body string
+}
+
 func main() {
 	flag.Parse()
 	fmt.Println("Creating a note with", *lineNumbers, "line numbers.")
+	fmt.Println("List flag ", *listFlag)
 
 	// Load all the strings into a process
-	ch := make(chan string)
+	// TODO need to get the strings via non params as well
+	ch := make(chan CommandInput)
 	scanner := bufio.NewScanner(os.Stdin)
 	go func() {
 		for scanner.Scan() {
-			ch <- scanner.Text()
+			ch <- CommandInput{*tagFlag, scanner.Text()}
 		}
 		if err := scanner.Err(); err != nil {
 			fmt.Fprintln(os.Stderr, "reading standard input:", err)
@@ -41,22 +50,28 @@ func main() {
 	// Process all the strings from the std input
 	var max = *lineNumbers
 	var count int = 0
+	var tags string
+
 loop:
 	for {
 		count++
 
 		select {
-		case s, ok := <-ch:
+		case commandInput, ok := <-ch:
 			if !ok {
+				fmt.Println("saved note. <id here>")
 				break loop
 			}
-			fmt.Println(s)
+			tags = commandInput.tags
+			fmt.Println(commandInput.body)
 		}
 		if count > max {
 			count = 0
 			fmt.Println("reached max lines ")
+			break
 		}
 
 	}
+	fmt.Print(tags)
 	fmt.Println("saved note. <id here>")
 }
